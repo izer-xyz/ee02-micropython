63d7ea0305ba6146ca4e71633915aa1929371889
Péter <peter@perenyi.net>
2026-09-26 09:50:04 +1000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-println("Hello EE02.")
+from ee02 import Ee02
+
+print("Hello EE02.")
+
+display = Ee02()
+display.begin()
+display.set_rotation(1)
+display.print_text(50, 50, "Partial update demo")
+display.draw_rect(50, 120, 150, 60, display.BLACK)
+display.display()
