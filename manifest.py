0139773e15db2951df3b4273dbf1f5fa84f3ba6b
Péter <peter@perenyi.net>
2026-09-25 22:16:10 +1000

    Update manifest.py

--- manifest.py
+++ manifest.py
@@ -1,2 +1,2 @@
 c_module("$(MPY_DIR)/../Inkplate-micropython/firmware/usermods/inkplate")
-module("inkplate13_spectra.py", base_path="Inkplate-micropython/boards/inkplate13spectra")
+module("inkplate13_spectra.py", base_path="$(MPY_DIR)/../Inkplate-micropython/boards/inkplate13spectra")
