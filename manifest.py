2a4b4b232b57bd1c128f6bc2ee5f8150fcba4e55
Péter <peter@perenyi.net>
2026-09-25 22:50:15 +1000

    Update manifest.py

--- manifest.py
+++ manifest.py
@@ -1,2 +1,7 @@
 c_module("$(MPY_DIR)/../Inkplate-micropython/firmware/usermods/inkplate")
 module("inkplate13_spectra.py", base_path="$(MPY_DIR)/../Inkplate-micropython/boards/inkplate13spectra")
+
+freeze("Inkplate-micropython/shared/drivers")
+freeze("Inkplate-micropython/shared/mixins")
+
+module("gfx_standard_font_01.py", base_path="Inkplate-micropython/boards/inkplate13spectra")
